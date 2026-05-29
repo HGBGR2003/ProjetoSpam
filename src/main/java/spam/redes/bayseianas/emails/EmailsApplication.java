349c37e98e27ea0ed34790565822ea82440f9ab3
= <=>
2026-05-29 15:15:49 -0300

    Mandando

---
+++ src/main/java/spam/redes/bayseianas/emails/EmailsApplication.java
@@ -0,0 +1,13 @@
+package spam.redes.bayseianas.emails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spam/redes/bayseianas/emails/EmailsApplicationTests.java
@@ -0,0 +1,13 @@
+package spam.redes.bayseianas.emails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
